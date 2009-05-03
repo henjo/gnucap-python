012517138f9e8882197daa0999016fe90b48f92b
Henrik Johansson <henjo2006@gmail.com>
2009-05-03 17:43:59 +0200

    Added gnucap.cc

---
+++ gnucap.cc
@@ -0,0 +1,21 @@
+#include "gnucap/u_lang.h"
+#include "gnucap/c_comand.h"
+#include "gnucap/globals.h"
+#include "gnucap/m_wave.h"
+#include "gnucap/s__.h"
+
+#include "numpy_interface.h"
+
+#include <string>
+
+void command(char *command) {
+  CMD::command(std::string(command), &CARD_LIST::card_list);
+}
+
+DISPATCHER<CMD>::INSTALL *attach_command(char *command_name, CMD *cmd) {
+  return new DISPATCHER<CMD>::INSTALL(&command_dispatcher, command_name, cmd);
+}
+
+void detach_command(DISPATCHER<CMD>::INSTALL *installer) {
+  delete installer;
+}
