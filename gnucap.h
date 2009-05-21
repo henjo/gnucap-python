2c4bd984dfe4a08e9c465e9d1e326d9995928575
Henrik Johansson <henjo2006@gmail.com>
2009-05-21 10:02:49 +0200

    Created a wrapper for SIM that otherwise was not wrapped by Swig. The reason is virtual private methods I think.

--- gnucap.h
+++ gnucap.h
@@ -1,5 +1,13 @@
 #include "gnucap/c_comand.h"
 #include "gnucap/l_dispatcher.h"
+#include "gnucap/s__.h"
+
+class SIMWrapper : public SIM {
+public:
+  explicit SIMWrapper():SIM()  {}
+  virtual void  setup(CS&)=0;
+  virtual void  sweep()=0;
+};
 
 void command(char *command);
 DISPATCHER<CMD>::INSTALL *attach_command(char *command_name, CMD *cmd);
