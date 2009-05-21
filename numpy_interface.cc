c8a4df43d37ba2ab7dde2169beef4cd098552335
Henrik Johansson <henjo2006@gmail.com>
2009-05-21 09:49:57 +0200

    Added more gnucap BSMatrix -> numpy array access functions Added a new forward-backward substituion function where the RHS and result is a numpy array

--- numpy_interface.cc
+++ numpy_interface.cc
@@ -1,4 +1,5 @@
-#include "gnucap/m_wave.h"
+#include <gnucap/m_matrix.h>
+#include <gnucap/m_wave.h>
 #include "numpy_interface.h"
 #include <Python.h>
 #include <numpy/arrayobject.h>
@@ -7,9 +8,42 @@ void init_numpy() {
   import_array();
 }
 
+PyObject *to_double_array(double *data, int len) {
+  npy_intp dims[1] = {len};
+  return PyArray_SimpleNewFromData(1, dims, PyArray_DOUBLE, (void *)(data+1));
+}
+
+PyObject *get_complex_array(COMPLEX *data, int len) {
+  npy_intp dims[1] = {len};
+  PyObject *arr = PyArray_SimpleNew(1, dims, PyArray_COMPLEX128);
+
+  npy_complex128 *outdata = (npy_complex128 *) PyArray_DATA(arr);
+  
+  // copy data
+  for(int i=0; i < len; i++) {
+    outdata[i].real = data[i+1].real();
+    outdata[i].imag = data[i+1].imag();
+  }
+  return arr;
+}
+
+void set_complex_array(COMPLEX *data, PyObject *srcarray) {
+  if( !PyArray_Check(srcarray) ||
+       ( PyArray_TYPE(srcarray ) != PyArray_COMPLEX128 ) ||
+      ( PyArray_NDIM(srcarray ) != 1 ))
+    throw Exception("Source array has incorrect type");
+ 
+  npy_complex128 *indata = (npy_complex128 *) PyArray_DATA(srcarray);
+  for(int i=0; i < PyArray_DIM(srcarray, 0); i++)
+    data[i] = COMPLEX(indata[i].real, indata[i].imag);
+}
+
 PyObject *wave_to_arrays(WAVE *wave) {
   PyObject *x, *y;
   int i, nrows = 0;
+
+  if(wave == NULL) 
+    throw Exception("wave_to_arrays got NULL instead of WAVE pointer");
 
   // Count number of elements
   // FIXME, WAVE should provide a method for this
@@ -36,5 +70,41 @@ PyObject *wave_to_arrays(WAVE *wave) {
   PyTuple_SetItem(pTuple, 1, y);
 
   return pTuple;
+}
+
+PyObject *bsmatrix_to_array_d(BSMATRIX<double> &A) {
+  PyObject *Aarray;
+  int i,j;
+
+  // Create arrays
+  npy_intp dims[2] = {A.size(), A.size()};
+  Aarray = PyArray_SimpleNew(2, dims, PyArray_DOUBLE);
+  
+  // Copy data
+  double *ptr = (double *) PyArray_DATA(Aarray);
+  for(i = 1; i <= A.size(); i++) 
+    for(j = 1; j <= A.size(); j++)
+      *ptr++ = A.m(i,j);
+
+  return Aarray;
+}
+
+PyObject *bsmatrix_to_array_c(BSMATRIX<COMPLEX> &A) {
+  PyObject *Aarray;
+  int i,j;
+
+  // Create arrays
+  npy_intp dims[2] = {A.size(), A.size()};
+  Aarray = PyArray_SimpleNew(2, dims, PyArray_COMPLEX128);
+  
+  // Copy data
+  npy_complex128 *ptr = (npy_complex128 *) PyArray_DATA(Aarray);
+  for(i = 1; i <= A.size(); i++) 
+    for(j = 1; j <= A.size(); j++) {
+      ptr->real = A.m(i,j).real();
+      ptr->imag = A.m(i,j).imag();
+      ptr++;
+    }
+  return Aarray;
 }
 
--- numpy_interface.h
+++ numpy_interface.h
@@ -1,6 +1,63 @@
+#include <gnucap/m_matrix.h>
 #include <Python.h>
+#include <numpy/arrayobject.h>
+
+PyObject *to_double_array(double *data, int len);
+PyObject *get_complex_array(COMPLEX *data, int len);
+void set_complex_array(COMPLEX *data, PyObject *srcarray);
 
 PyObject *wave_to_arrays(WAVE *wave);
+PyObject *bsmatrix_to_array_d(BSMATRIX<double> &A);
+PyObject *bsmatrix_to_array_c(BSMATRIX<COMPLEX> &A);
 void init_numpy();
+
+// Function for performing fb substitution of a BSMATRIX with
+// a numpy array as the rhs vector or matrix
+template <class T>
+void bsmatrix_fbsub_array(BSMATRIX<T> *A, PyObject *rhs, PyObject *dest) {
+/*   if( !PyArray_Check(rhs) || */
+/*       ( PyArray_NDIM(rhs ) > 2) || */
+/*       ( PyArray_NDIM(rhs ) < 1) */
+/*       ) */
+/*     throw Exception("Dest array has incorrect type"); */
+/*   if( !PyArray_Check(dest) || */
+/*       ( PyArray_NDIM(dest) != PyArray_NDIM(rhs))) */
+/*     throw Exception("Dest array has incorrect type"); */
+
+  int rows = PyArray_DIM(rhs, 0);
+
+  // RHS and solution vector
+  T v[rows+1];
+
+  if(PyArray_NDIM(rhs) == 1) {
+    // RHS is a vector
+
+    // Copy rhs to v
+    for(int i=0; i < rows; i++)
+      v[i+1] = *(T *)PyArray_GETPTR1(rhs, i);
+
+    A->fbsub(v);
+
+    // Copy result to dest array
+    for(int i=0; i < rows; i++)
+      *(T *)PyArray_GETPTR1(dest, i) = v[i+1];
+  } else {
+    // RHS is a matrix
+    int cols = PyArray_DIM(rhs, 1);
+
+    for(int col=0; col < cols; col++) {
+      // Copy rhs to v
+      for(int i=0; i < rows; i++)
+        v[i+1] = *(T *)PyArray_GETPTR2(rhs, i, col);
+
+      A->fbsub(v);
+      
+      // Copy result to dest array
+      for(int i=0; i < rows; i++)
+        *(T *)PyArray_GETPTR2(dest, i, col) = v[i+1];
+    }
+  }
+}
+
 
 
